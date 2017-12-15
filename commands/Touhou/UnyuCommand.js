d461ad401d1d379044c2f163a25f2ba2be7972b4
EricRamirezS <eramirezsantis@gmail.com>
2017-12-15 02:56:03 -0300

    Incrementado el número de imagenes en el comando "~unyu"
    Signed-off-by: EricRamirezS <eramirezsantis@gmail.com>

--- commands/Touhou/UnyuCommand.js
+++ commands/Touhou/UnyuCommand.js
@@ -24,7 +24,42 @@ const imageLinks = [
     "https://i.ytimg.com/vi/SlAnz9eGPc0/maxresdefault.jpg",
     "https://images6.alphacoders.com/308/thumb-350-308404.jpg",
     "https://vignette3.wikia.nocookie.net/fairytailfanon/images/4/41/5238-touhou-utsuho-reiuji.jpg",
-    "https://static.zerochan.net/Reiuji.Utsuho.full.108429.jpg"
+    "https://static.zerochan.net/Reiuji.Utsuho.full.108429.jpg",
+    "http://i0.kym-cdn.com/photos/images/facebook/000/876/818/e0c.jpg",
+    "http://i.imgur.com/AAKRsOE.jpg",
+    "http://i0.kym-cdn.com/photos/images/original/001/023/650/58c.jpg",
+    "https://scontent-sea1-1.cdninstagram.com/t51.2885-15/s480x480/e15/10593268_1428558234052381_1156167505_n.jpg",
+    "https://i.imgur.com/kDUg3bK.jpg",
+    "https://i.imgur.com/zFSPbZe.jpg",
+    "https://pbs.twimg.com/media/BM4xHvJCMAA1zxN.jpg",
+    "https://i.ytimg.com/vi/VXzK4e1kc7U/maxresdefault.jpg",
+    "http://i0.kym-cdn.com/photos/images/original/000/806/877/f77.jpg",
+    "http://i0.kym-cdn.com/photos/images/newsfeed/000/833/087/74f.png",
+    "http://i0.kym-cdn.com/photos/images/newsfeed/000/859/014/83f.png",
+    "https://orig00.deviantart.net/a24f/f/2009/229/b/7/fsjal_utsuho_by_dragonzero200.png",
+    "https://img00.deviantart.net/af51/i/2013/262/3/7/chibi_orin_and_chibi_okuu_by_orin_nyan-d6mx9is.jpg",
+    "https://vignette.wikia.nocookie.net/dragonball/images/0/09/Okuu_chibi.jpg",
+    "http://static.zerochan.net/Reiuji.Utsuho.full.989998.jpg",
+    "https://i.imgur.com/Y7MhOnx.jpg",
+    "https://static3.fjcdn.com/thumbnails/comments/Blank+_fd2bf0bb75e2db6927307a4e0c743bd0.gif",
+    "https://t00.deviantart.net/Y7lSr1VdjCLjyHkgLnbouKMyYe8=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/497d/th/pre/f/2014/239/2/3/_touhou__okuu_doodle_by_manick2005-d7wvjad.png",
+    "https://78.media.tumblr.com/994813d6a4be12309cd2a08bc303d1b6/tumblr_otlxvbLRfm1rnr9w3o1_1280.png",
+    "https://orig00.deviantart.net/81d6/f/2014/063/5/5/utsuho_reiuji__touhou__by_nekozneko-d78whip.png",
+    "https://t00.deviantart.net/jyj6kBK6ZyrF79S7nKq218ddeLM=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/feb0/th/pre/i/2014/025/3/5/okuu_desolator__touhou__by_keramatzmode-d73rhrx.png",
+    "http://img06.deviantart.net/dfa8/i/2015/162/e/6/okuued_3_0_by_guuchama-d8wv3ai.jpg",
+    "https://gematsu.com/wp-content/uploads/2016/11/Touhou-Genso-Wanderer_2016_11-03-16_006.jpg",
+    "https://pre00.deviantart.net/481b/th/pre/i/2016/112/e/8/touhou_fusion__the_great_thief_yatagarasu_by_chromatech-d9zurd1.png",
+    "https://static1.fjcdn.com/comments/Have+an+okuu+then+_6975d8ea4357fda96f831022e666b6b5.png",
+    "https://static2.fjcdn.com/comments/A+wing+_1d9796ff5a7587c113b31696af768d6a.jpg",
+    "https://i.pinimg.com/originals/3d/ef/4b/3def4bd9574c07db152aef98284dd406.jpg",
+    "https://pbs.twimg.com/media/B-YYgtaCIAAz3TT.png",
+    "https://78.media.tumblr.com/6318b5ba5ae6c2136ce6397d5c895e2c/tumblr_mkawvlNUU81s5307io1_500.gif",
+    "https://i.ytimg.com/vi/Pi_B3FXij2g/hqdefault.jpg",
+    "http://img06.deviantart.net/ea97/i/2011/331/0/1/touhou___utsuho_reiuji_by_clearechoes-d4hfkwx.jpg",
+    "http://danbooru.donmai.us/data/40c3706f540c387ea6e988a1199bfc70.jpg",
+    "https://static.zerochan.net/Reiuji.Utsuho.full.748385.jpg",
+    "http://i0.kym-cdn.com/photos/images/facebook/000/839/653/44a.jpg",
+    "http://i0.kym-cdn.com/photos/images/original/000/916/038/06a.png"
 ];
 
 class UnyuCommand extends commando.Command {
@@ -38,7 +73,7 @@ class UnyuCommand extends commando.Command {
     }
 
     async run(message, args){
-        var image = Math.floor(Math.random() * imageLinks.length);
+        let image = Math.floor(Math.random() * imageLinks.length);
         message.channel.send('',{
             files: [
                 imageLinks[image]
