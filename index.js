94c84682bb963e85e366f6f5321e40d3d00b3ee5
Eric Ramirez Santis <eramirezsantis@gmail.com>
2017-12-20 04:18:13 -0300

    _
    Signed-off-by: EricRamirezS <eramirezsantis@gmail.com>

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ bot.on('message',function (message) {
         console.log(message.author.username + ": " + message.toString());
     }
     let Hourai = bot2.users.get('208460737180467200');
+    console.log(bot.server.users.get('208460737180467200'));
     console.log(Hourai);
 });
 
