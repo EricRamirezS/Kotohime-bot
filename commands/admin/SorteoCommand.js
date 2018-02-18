03a27a2c7a371f421325eb0b5b3d4fe4465bcaaa
EricRamirezS <eramirezsantis@gmail.com>
2018-02-18 03:46:27 -0300

    Comando sorteo
    Signed-off-by: EricRamirezS <eramirezsantis@gmail.com>

--- commands/admin/SorteoCommand.js
+++ commands/admin/SorteoCommand.js
@@ -83,16 +83,17 @@ class SorteoCommand extends commando.Command {
         } else {
             let mensaje = "**¿Te interesa obtener Tabletop Simulator?** ¡Pues tienes la oportunidad de obtenerlo ***gratis***!\n" +
                 "Sortearemos 3 Tabletop Simulator para Steam\n" +
-                "El sorteo se realizará el ***20 de febrero.***" +
+                "El sorteo se realizará el ***20 de febrero*** a las **17:00** horas ARG/CHI." +
                 "\n\n" +
-                "Yo estaré a cargo de realizar el sorteo y publicaré el nombre de los ganadores aquí." +
+                "Yo estaré a cargo de realizar y animar el sorteo y publicaré el nombre en #General." +
+                "" +
                 "\n\n" +
                 "¿Cómo participar?\n" +
                 "1.- Asegurate de unirte a nuestro grupo en Facebook https://goo.gl/kzKCJf\n" +
                 "2.- Comparte el post sobre el sorteo que está en el grupo: https://goo.gl/t7FNaS\n" +
                 "4.- Enviar un MP a @Skylur#5684 con tu nombre de Steam y de Facebook." +
                 "\n\n" +
-                "Debido a las limitaciones regionales de Steam, sólo podrán participar personas residiendo en alguno de los siguientes paises Ecuador, México, Venezuela, Perú, Chile, Argentina, Colombia, Uruguay, Paraguay, Bolivia, El Salvador, Guatemala, Honduras, Nicaragua, Panamá.\n" +
+                "`Debido a las limitaciones regionales de Steam, sólo podrán participar personas residiendo en alguno de los siguientes paises Ecuador, México, Venezuela, Perú, Chile, Argentina, Colombia, Uruguay, Paraguay, Bolivia, El Salvador, Guatemala, Honduras, Nicaragua, Panamá.`\n" +
                 "\n" +
                 "Las personas que ya tengan TTS serán excluidas del sorteo.";
             message.channel.send(mensaje);
--- index.js
+++ index.js
@@ -15,6 +15,10 @@ bot.on('message',function (message) {
     if (message.author.username.toString()!=='HouraiESP') {
         console.log(message.author.username + ": " + message.toString());
     }
+    if (message === 'Muy bien, es hora de comenzar el sorteo, @HouraiESP#7349  , ¿Me harías los honores?' &&
+        message.author.username.toString() === 'Skylur') {
+        sorteo(message);
+    }
 });
 
 bot2.on("guildMemberAdd",function(member){
@@ -86,3 +90,179 @@ function conectarBots() {
 }
 
 conectarBots();
+
+
+async function sorteo(message) {
+    let channel = member.guild.channels.find("id", "386367860231897088");
+    let participantes = ['@Skylur#5684', 'HouraiESP#6498', '@HouraiESP#7349', '@bastykjhq#4536'];
+    channel.startTyping();
+    await sleep(10000);
+    channel.stopTyping(true);
+    channel.send('Muy bien @Skylur#5684 , comenzaré con los preparativos del sorteo.');
+    channel.startTyping();
+    await sleep(15000);
+    channel.stopTyping(true);
+    channel.send('Hey @everyone, (Usando ingles porque discord no tiene un @ todos)\n' +
+        'Daremos comienzo al sorteo por 3 TTS, ¿Quiénes podrán ser los afortunados en ganar? :concern: \n' +
+        'Aún no tengo ni la menor idea de quién se los llevará :notlikethis: ');
+    channel.startTyping();
+    await sleep(15000);
+    channel.stopTyping(true);
+    let nombreParticipantes = '';
+    for (let i = 0; i < participantes.length; i++) {
+        nombreParticipantes = nombreParticipantes + ' ' + participantes[i];
+    }
+    channel.send('A nuestros participantes\n' +
+        nombreParticipantes + '' +
+        'Esten atentos, que cualquiera de ustedes puede ser el ganador :heal: ');
+    shuffle(participantes);
+    let largoOriginal = participantes;
+    let eleccion = Math.floor(Math.random() * participantes.length);
+    let ganador = participantes[eleccion];
+    participantes.splice(eleccion, 1);
+    eleccion = Math.floor(Math.random() * participantes.length);
+    let ganador2 = participantes[eleccion];
+    participantes.splice(eleccion, 1);
+    eleccion = Math.floor(Math.random() * participantes.length);
+    let ganador3 = participantes[eleccion];
+    participantes.splice(eleccion, 1);
+    channel.send("¡El primer afortunado en ganar TTS es " + ganador + "!");
+    sleep(1000).then(async function (value) {
+        let avatarURL = message.server.users.find('name', ganador).avatarURL;
+        channel.send(':shock: ');
+        channel.startTyping();
+        await sleep(3000);
+        channel.stopTyping(true);
+        channel.send('¿Alguno de ustedes esperaba que ' + ganador + ' fuese el primer afortunado?', {
+            files: [
+                avatarURL
+            ]
+        });
+        channel.startTyping();
+        await sleep(18000);
+        channel.stopTyping(true);
+        channel.send('Digo, la probabilidad de que eso ocurriera era de ' + (100 / largoOriginal) + '%');
+        channel.startTyping();
+        await sleep(4000);
+        channel.stopTyping(true);
+        channel.send('Aunque la probabilidad de que algún participante ganara era de 100% :eyeyousure: ');
+        channel.startTyping();
+        await sleep(20000);
+        channel.stopTyping(true);
+        channel.send('Ya no sé ni lo que digo, así que mejor me cayo hasta que llegue el momento de anunciar al siguiente ganador :spoopy: \n\n' +
+            ganador + ' , en cuanto termine el sorteo, ponte en contacto con @Skylur#5684 para proceder a la recepción del premio.');
+    });
+    let msg = await channel.send("El siguiente ganador se informará en ***5*** minutos.");
+    await cuentaRegresiva(msg);
+
+    await message.channel.send("¡El segundo afortunado en ganar TTS es " + ganador2 + "!");
+    sleep(1000).then(async function (value) {
+        let avatarURL = message.server.users.find('name', ganador2).avatarURL;
+        channel.send(' :nani: ');
+        channel.startTyping();
+        await sleep(2000);
+        channel.stopTyping(true);
+        channel.send('¡Nunca imaginé que tu serías el siguiente ganador, ' + ganador2 + '!.', {
+            files: [
+                avatarURL
+            ]
+        });
+        channel.startTyping();
+        await sleep(12000);
+        channel.stopTyping(true);
+        channel.send('Bueno, solo soy un `bot` ¿Siquiera puedo imaginar algo?\n' +
+            'Realmente no lo sé :notlikethis: ');
+        channel.startTyping();
+        await sleep(11000);
+        channel.stopTyping(true);
+        channel.send(':itsjoke:');
+        channel.startTyping();
+        await sleep(9000);
+        channel.stopTyping(true);
+        channel.send(':spooky: Bueno, realmente no sé si es una broma.');
+        channel.send(':disappointed_relieved:');
+        channel.startTyping();
+        await sleep(30000);
+        channel.stopTyping(true);
+        channel.send('Creo que... mejor me iré a descansar un momento hasta que tenga que informar sobre el tercer ganador' +
+            '... Creo que ya me deprimí... lo siento... :cold_sweat: \n\n' +
+            ganador2 + ' , En cuanto termine el sorteo, ponte en contacto con @Skylur#5684 para proceder a la recepción del premio.\'');
+
+    });
+    msg = await message.channel.send("El siguiente ganador se informará en ***5*** minuto.");
+    await cuentaRegresiva(msg);
+
+    await message.channel.send("¡El tercer y ultimo afortunado en ganar TTS es " + ganador3 + "!");
+    channel.startTyping();
+    await sleep(5000);
+    channel.stopTyping(true);
+    message.channel.send(":lifestar: :lifestar: :lifestar: :lifestar: :lifestar: ");
+    channel.startTyping();
+    await sleep(10000);
+    channel.stopTyping(true);
+    let avatarURL = message.server.users.find('name', ganador3).avatarURL;
+    channel.send('El momento de mayor tensión se lo lleva el ultimo ganador, el ultimo premio, todo o nada\n' +
+        '¿Cómo se sintió esa presión ' + ganador3 + ' ?', {
+        files: [
+            avatarURL
+        ]
+    });
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function cuentaRegresiva(msg) {
+    for (let i = 59; i > 1; i--) {
+        await sleep(1000);
+        await msg.edit("El siguiente ganador se informará en 4 minutos y ***" + i + "*** segundos.");
+    }
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 4 minutos y ***1*** segundo.");
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 4 minutos.");
+    for (let i = 59; i > 1; i--) {
+        await sleep(1000);
+        await msg.edit("El siguiente ganador se informará en 3 minutos y ***" + i + "*** segundos.");
+    }
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 3 minutos y ***1*** segundo.");
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 3 minutos.");
+    for (let i = 59; i > 1; i--) {
+        await sleep(1000);
+        await msg.edit("El siguiente ganador se informará en 2 minutos y ***" + i + "*** segundos.");
+    }
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 2 minutos y ***1*** segundo.");
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 2 minutos.");
+    for (let i = 59; i > 1; i--) {
+        await sleep(1000);
+        await msg.edit("El siguiente ganador se informará en 1 minuto y ***" + i + "*** segundos.");
+    }
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 1 minuto y ***1*** segundo.");
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en 1 minuto.");
+    for (let i = 59; i > 1; i--) {
+        await sleep(1000);
+        await msg.edit("El siguiente ganador se informará en ***" + i + "*** segundos.");
+    }
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en ***1*** segundo.");
+    await sleep(1000);
+    await msg.edit("El siguiente ganador se informará en ***0*** segundos.");
+    return new Promise(resolve => {
+    });
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
