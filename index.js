b3b0d824875e0f9cc12bca5b02be736d9b320153
Eric Ramirez Santis <eramirezsantis@gmail.com>
2018-02-18 08:21:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ bot.on('message',function (message) {
 });
 
 bot2.on("guildMemberAdd",function(member){
-    let chan = member.guild.channels.find("name","voz");
+    let chan = member.guild.channels.find("id","385996736990281730");
     chan.send(member+" se ha unido al servidor.");
 });
 
 bot2.on("guildMemberRemove",function(member){
-    var chan = member.guild.channels.find("name","voz");
+    var chan = member.guild.channels.find("id","385996736990281730");
     chan.send(member+" ha dejado el servidor.");
 });
 
