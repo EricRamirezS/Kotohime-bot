f5e129091d2a8a19a1d4f68443b18e96a5104636
EricRamirezS <eramirezsantis@gmail.com>
2018-02-19 06:54:14 -0300

    Rol
    Signed-off-by: EricRamirezS <eramirezsantis@gmail.com>

--- commands/admin/RoleCommand.js
+++ commands/admin/RoleCommand.js
@@ -26,92 +26,102 @@ class RoleCommand extends commando.Command {
         });
     }
 
-    async run(message, args){
-        try{
-            var accion = args.accion.toLowerCase();
-            switch (accion){
-                case "listar":
-                    var mensaje = "__Puedes escoger uno o más de los siguientes roles:__\n\n" +
-                        "**Danmaku-Ahora**: Para aquellos usuarios que quieren jugar una partida de Danmaku en Tabletop Simulator en este preciso momento.\n" +
-                        "**Danmaku-Sudamerica**: Para usuarios que viven en sudamérica\n" +
-                        "**Danmaku-Europa**:Para usuarios que viven en Europa.\n" +
-                        "**Danmaku-Norteamerica**:Para usuarios que viven en Nortemaérica.\n" +
-                        "**Danmaku-Preguntar**: Para usuarios que desean jugar una partida en Tabletop Simulator, pero no están seguros si pueden en este precismo momento.\n\n" +
-                        "Para adquirir uno de estos roles, utiliza '~rol obtener nombre'\n" +
-                        "Para abandonar uno de estos roles, utiliza '~rol abandonar nombre'\n";
-                    message.channel.send(mensaje);
-                    break;
-                case "obtener":
-                    try{
-                        var nombreRol = args.Nombre_Rol.toLowerCase();
-                        switch (nombreRol) {
-                            case "danmaku-las":
-                            case "danmaku-eu":
-                            case "danmaku-na":
-                            case "danmaku-sea":
-                                let mensajeFinal =
-                                    nombreRol.split("-")[0].charAt(0).toLowerCase()
-                                    +
-                                    "-"
-                                    +
-                                    nombreRol.split("-")[1].toUpperCase()
-                                let role = message.guild.roles.find("name", mensajeFinal);
-                                let guildMember = message.guild.members.get(message.author.id + '');
-                                guildMember.addRole(role).then(function (value) {
-                                    message.channel.send(":thumbsup:")
-                                });
-                                break;
-                            case "danmaku-preguntar":
-                            case "danmaku-ahora":
-                                let role = message.guild.roles.find("name", args.Nombre_Rol.toLowerCase());
-                                let guildMember = message.guild.members.get(message.author.id + '');
-                                guildMember.addRole().then(function (value) {
-                                    message.channel.send(":thumbsup:")
-                                });
-                            default: throw new Error();
+    async run(message, args) {
+        if (args.accion) {
+            try {
+                let accion = args.accion.toLowerCase();
+                switch (accion) {
+                    case "listar":
+                        let mensaje = "__Puedes escoger uno o más de los siguientes roles:__\n\n" +
+                            "**Danmaku-Ahora**: Para aquellos usuarios que quieren jugar una partida de Danmaku en Tabletop Simulator en este preciso momento.\n" +
+                            "**Danmaku-Sudamerica**: Para usuarios que viven en sudamérica\n" +
+                            "**Danmaku-Europa**:Para usuarios que viven en Europa.\n" +
+                            "**Danmaku-Norteamerica**:Para usuarios que viven en Nortemaérica.\n" +
+                            "**Danmaku-Preguntar**: Para usuarios que desean jugar una partida en Tabletop Simulator, pero no están seguros si pueden en este precismo momento.\n\n" +
+                            "Para adquirir uno de estos roles, utiliza '~rol obtener nombre'\n" +
+                            "Para abandonar uno de estos roles, utiliza '~rol abandonar nombre'\n";
+                        message.channel.send(mensaje);
+                        break;
+                    case "obtener":
+                        try {
+                            let nombreRol = args.Nombre_Rol.toLowerCase();
+                            switch (nombreRol) {
+                                case "danmaku-las":
+                                case "danmaku-eu":
+                                case "danmaku-na":
+                                case "danmaku-sea":
+                                    let mensajeFinal =
+                                        nombreRol.split("-")[0].charAt(0).toLowerCase()
+                                        +
+                                        "-"
+                                        +
+                                        nombreRol.split("-")[1].toUpperCase()
+                                    let role = message.guild.roles.find("name", mensajeFinal);
+                                    let guildMember = message.guild.members.get(message.author.id + '');
+                                    guildMember.addRole(role).then(function (value) {
+                                        message.channel.send(":thumbsup:")
+                                    });
+                                    break;
+                                case "danmaku-preguntar":
+                                case "danmaku-ahora":
+                                    let role = message.guild.roles.find("name", args.Nombre_Rol.toLowerCase());
+                                    let guildMember = message.guild.members.get(message.author.id + '');
+                                    guildMember.addRole().then(function (value) {
+                                        message.channel.send(":thumbsup:")
+                                    });
+                                    break;
+                                default:
+                                    throw new Error();
+                            }
                         }
-                    }
-                    catch (e){
-                        console.log(e.toString());
-                        message.channel.send("No has ingresado el nombre del rol o el nombre es incorrecto");
-                    }
-                    break;
-                case "abandonar": try{
-                    var nombreRol = args.Nombre_Rol.toLowerCase();
-                    switch (nombreRol) {
-                        case "danmaku-ahora":
-                        case "danmaku-las":
-                        case "danmaku-eu":
-                        case "danmaku-na":
-                        case "danmaku-sea":
-                        case "danmaku-preguntar":
-                            var mensajeFinal =
-                                nombreRol.split("-")[0].charAt(0).toUpperCase()
-                                +
-                                nombreRol.split("-")[0].slice(1)
-                                +
-                                "-"
-                                +
-                                nombreRol.split("-")[1].charAt(0).toUpperCase()
-                                +
-                                nombreRol.split("-")[1].slice(1);
-                            var role = message.guild.roles.find("name",mensajeFinal);
-                            var guildMember = message.guild.members.get(message.author.id+'');
-                            guildMember.removeRole(role);
-                            message.channel.send(":thumbsup:");
-                            break;
-                        default: throw new Error();
-                    }
+                        catch (e) {
+                            console.log(e.toString());
+                            message.channel.send("No has ingresado el nombre del rol o el nombre es incorrecto");
+                        }
+                        break;
+                    case "abandonar":
+                        try {
+                            let nombreRol = args.Nombre_Rol.toLowerCase();
+                            switch (nombreRol) {
+                                case "danmaku-las":
+                                case "danmaku-eu":
+                                case "danmaku-na":
+                                case "danmaku-sea":
+                                    let mensajeFinal =
+                                        nombreRol.split("-")[0].charAt(0).toLowerCase()
+                                        +
+                                        "-"
+                                        +
+                                        nombreRol.split("-")[1].toUpperCase()
+                                    let role = message.guild.roles.find("name", mensajeFinal);
+                                    let guildMember = message.guild.members.get(message.author.id + '');
+                                    guildMember.removeRole(role).then(function (value) {
+                                        message.channel.send(":thumbsup:")
+                                    });
+                                    break;
+                                case "danmaku-preguntar":
+                                case "danmaku-ahora":
+                                    let role = message.guild.roles.find("name", args.Nombre_Rol.toLowerCase());
+                                    let guildMember = message.guild.members.get(message.author.id + '');
+                                    guildMember.removeRole().then(function (value) {
+                                        message.channel.send(":thumbsup:")
+                                    });
+                                default:
+                                    throw new Error();
+                            }
+                        }
+                        catch (e) {
+                            console.log(e.toString());
+                            message.channel.send("No has ingresado el nombre del rol o el nombre es incorrecto");
+                        }
+                        break;
+                    default:
+                        message.channel.send("La acción ingresada no es valida");
                 }
-                catch (e){
-                    console.log(e.toString());
-                    message.channel.send("No has ingresado el nombre del rol o el nombre es incorrecto");
-                }break;
-                default: message.channel.send("La acción ingresada no es valida");
             }
-        }
-        catch (err){
-            console.log("sad");
+            catch (err) {
+                console.log("sad");
+            }
         }
     }
 }
