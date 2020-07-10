0d17f0d5e8c376c09c31e1a711c9c717f06d4569
EricRamirezs <eramirezsantis@gmail.com>
2020-07-10 15:28:20 -0400

    Corregido Comandos deben comnezar con minuscula

---
+++ commands/misc/SafebooruCommand.js
@@ -0,0 +1,32 @@
+const commando = require('discord.js-commando');
+const safebooru = require('../../safebooruImage2Channel');
+
+class SafebooruCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'safebooru',
+            group: 'misc',
+            memberName: 'safebooru',
+            description: 'Dejame googlear eso por ti.',
+            args: [
+                {
+                    key: 'tags',
+                    prompt: 'Coloque los tags que desea buscar.',
+                    type: 'string',
+                    default: ""
+                }
+            ]
+        });
+    }
+
+    async run(message, args) {
+        if (args) {
+            let tags = args.replace(" ", "+");
+            safebooru(message, tags);
+        } else {
+            message.channel.send("El texto de entrada tiene muy pocos parametros.");
+        }
+    }
+}
+
+module.exports = SafebooruCommand;
