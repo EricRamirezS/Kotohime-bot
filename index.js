e40a459f11ea6f650645ed67c20b76bbd692fb14
EricRamirezs <eramirezsantis@gmail.com>
2020-07-11 01:13:43 -0400

    Cambiada la ubicación de los metodos onAction

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-const Discord = require('discord.js');
 const commando = require('discord.js-commando');
 const bot = new commando.Client(); //commando bot
+const setListenerFunctions = require('./listener/ListenersSet');
 
-
-//GLOBAL VARIABLES
-// Banned tags for image search
 
 bot.registry.registerGroup('admin', 'Admin');
 bot.registry.registerGroup('misc', 'Misc');
 bot.registry.registerGroup('comun', 'Comun');
-bot.registry.registerGroup('touhou', 'Touhou');
-bot.registry.registerGroup('personalizado', 'Personalizado');
+bot.registry.registerGroup('touhou', 'touhou');
+bot.registry.registerGroup('danmaku', 'danmaku!!');
+bot.registry.registerGroup('nosotros', 'DanmakuESP');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.on('message',function (message) {
-    if (message.author.username.toString()!=='HouraiESP') {
-        console.log(message.author.username + ": " + message.toString());
-    }
-});
-
-bot.on("guildMemberAdd",function(member){
-    let chan = member.guild.channels.find("id","386366248306343937");
-    chan.send(member.user.username +" se ha unido al servidor.");
-});
-
-bot.on("guildMemberRemove",function(member){
-    let chan = member.guild.channels.find("id", "386366248306343937");
-    chan.send(member.user.username +" ha dejado el servidor.");
-});
-
-bot.on("voiceStateUpdate", function (oldMember, newMember) {
-    let chan = newMember.guild.channels.find("id","385996736990281730");
-    let nombre = "**"+oldMember.user.username+"**";
-    let oldChannel = oldMember.voiceChannelID;
-    let newChannel = newMember.voiceChannelID;
-    if (oldChannel){
-        if(newChannel){
-            let oldChannelName = "**"+newMember.guild.channels.find("id",oldChannel+"").name+"**";
-            let newChannelName = "**"+newMember.guild.channels.find("id",newChannel+"").name+"**";
-            if (oldChannelName !== newChannelName){
-                chan.send(nombre+" se ha movido de "+oldChannelName+" a "+newChannelName);
-            }
-        }else {
-            let oldChannelName = "**"+newMember.guild.channels.find("id",oldChannel+"").name+"**";
-            chan.send(nombre+" se retiró de "+oldChannelName);
-        }
-    } else {
-        let newChannelName = "**"+newMember.guild.channels.find("id",newChannel+"").name+"**";
-        chan.send(nombre+" se unió a "+newChannelName);
-    }
-
-});
-
-bot.on('disconnect', function(erMsg, code) {
-    console.log('----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----');
-    conectarBot();
-});
+setListenerFunctions(bot);
 
 function conectarBot() {
     bot.login(process.env.BOT_TOKEN1).then(function () {
