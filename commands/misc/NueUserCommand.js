455998bdabe6e9b7c28ba24854975cc1844561c0
EricRamirezs <eramirezsantis@gmail.com>
2020-07-12 01:23:42 -0400

    Fixed parametro de clase args vacio congelaba al bot completo

--- commands/misc/NueUserCommand.js
+++ commands/misc/NueUserCommand.js
@@ -1,23 +1,18 @@
 const commando = require('discord.js-commando');
-
 const imageLinks = [
     "https://cdn.discordapp.com/attachments/386366248306343937/731681950712070214/nueser.jpg",
     "https://cdn.discordapp.com/attachments/386366248306343937/731681458682331267/Every_time_there_is_a_new_user.gif",
     "https://cdn.discordapp.com/attachments/274920985029902336/730448333792870481/Oh_hi_NUE.gif",
     "https://cdn.discordapp.com/attachments/274920985029902336/725697204974780436/Nue_User.png",
-
-
 ];
 
-class NueUserCommando extends commando.Command {
+class NueUserCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: 'nueuser',
             group: 'misc',
             memberName: 'nueuser',
-            description: 'Por favor, usame cuando tengamos un nuevo usuario.',
-            args: [
-            ]
+            description: 'Por favor, usame cuando tengamos un nuevo usuario.'
         });
     }
 
@@ -31,4 +26,4 @@ class NueUserCommando extends commando.Command {
     }
 }
 
-module.exports = NueUserCommando;
+module.exports = NueUserCommand;
