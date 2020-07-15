861ae6a429c6fbbec152c831bc33cef3d35cb5be
EricRamirezs <eramirezsantis@gmail.com>
2020-07-15 16:36:36 -0400

    removed auto set de nombre del bot

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const commando = require('discord.js-commando');
 const bot = new commando.Client(); //commando bot
 const setListenerFunctions = require('./listener/ListenersSet');
 
-const username = "Kotohime";
-const avatar = "https://cdn.discordapp.com/attachments/414736061890166794/733025621403500674/Sin_titulo.png";
+const avatar = "https://cdn.discordapp.com/avatars/386007907113762816/cb23b22070e056c422074c454bb6f76f.png?size=2048";
 
 bot.registry.registerGroup('admin', 'Admin');
 bot.registry.registerGroup('misc', 'Misc');
@@ -19,7 +18,6 @@ setListenerFunctions(bot);
 function conectarBot() {
     bot.login(process.env.BOT_TOKEN1).then(function () {
         console.log("Bot conectado");
-        setDatosBot();
     }).catch(e => {
         console.log("bot no conectado");
         console.log(e);
@@ -32,9 +30,6 @@ function conectarBot() {
 
 conectarBot();
 
-
-
-function setDatosBot() {
-    if (bot.user.avatarURL !== avatar) bot.user.setAvatar(avatar).then().catch();
-    if (bot.user.username !== username) bot.user.setUsername(username).then().catch();
-};
+bot.on('ready', function() {
+    if (bot.user.avatarURL !== avatar) bot.user.setAvatar(avatar);
+});
