7d05de6828595d2633b0b32c66b304c87264581b
EricRamirezs <eramirezsantis@gmail.com>
2020-07-23 00:25:04 -0400

    Registrando al guild en la db

--- listener/functions/guild/create/onGuildCreate.js
+++ listener/functions/guild/create/onGuildCreate.js
@@ -1,15 +1,11 @@
+const {registerGuild} = require('../../../db/DBUpdateGuildSetting');
+
 /**
  * Se emite cada vez que el cliente se une a un servidor.
  * @param guild El nuevo servidor
  * @see https://discord.js.org/#/docs/main/stable/class/Guild
  */
 module.exports = (guild) => {
-    //TODO
+    registerGuild(guild.id);
 };
 
-function registrarGUild(guild){
-    let query = {
-        text: 'INSERT INTO GUILD(GUILD_ID) values($1)',
-        values: [guild.id]
-    }
-}
