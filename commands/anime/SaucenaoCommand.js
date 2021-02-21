e5cf4d17d9911395f410e1e4f550911cad4303ea
EricRamirezs <eramirezsantis@gmail.com>
2021-02-20 23:43:32 -0300

    daily throttling

--- commands/anime/SaucenaoCommand.js
+++ commands/anime/SaucenaoCommand.js
@@ -2,13 +2,6 @@ const commando = require('discord.js-commando');
 const request = require('snekfetch');
 const Discord = require('discord.js');
 
-let DBS = {
-    pixiv: 5,
-    danbooru: 9,
-    gelbooru: 25,
-    konachan: 26,
-    sankaku: 27,
-}
 
 class SaucenaoCommand extends commando.Command {
     constructor(client) {
@@ -19,8 +12,8 @@ class SaucenaoCommand extends commando.Command {
             description: 'Dame una imagen y te diré su fuente.',
             examples: ['saucenao'],
             throttling: {
-                usages: 5,
-                duration: 30,
+                usages: 200,
+                duration: 86400,
             },
             args: [{
                 key: 'url',
@@ -66,54 +59,35 @@ async function searchImage(channel, url) {
     ]
 
     let res = [];
-    for (let i = 0; i < Object.keys(DBS).length; i++) {
-        await request.post(BASE_REQUEST + "db=" + DBS[Object.keys(DBS)[i]] + "&" + request_params.join("&"))
-            .send()
-            .then(r => {
-                let results = JSON.parse(r.text).results;
-                if (results) {
-                    for (let j = 0; j < results.length; j++) res.push(results[j]);
-                }
-            }).catch(e => {
-                    console.log(e);
-                }
-            );
-    }
-    let flag = true;
-    if (res.length > 0) {
-        res.sort(((a, b) => b.header.similarity - a.header.similarity))
-        flag = res[0].header.similarity > 50;
-    }
-
-    if (!flag) {
-        await request.post(BASE_REQUEST + "db=" + 999 + "&" + request_params.join("&"))
-            .send()
-            .then(r => {
-                let results = JSON.parse(r.text).results;
-                if (results) {
-                    for (let j = 0; j < results.length; j++) res.push(results[j]);
-                }
-            }).catch(e => {
-                    console.log(e);
-                    channel.send("¿Realmente me has enviado una imagen? Ha pasado algo inesperado.")
-                }
-            );
-    }
+    request.post(BASE_REQUEST + "db=" + 999 + "&" + request_params.join("&"))
+        .send()
+        .then(r => {
+            let results = JSON.parse(r.text).results;
+            if (results) {
+                for (let j = 0; j < results.length; j++) res.push(results[j]);
+            }
+        }).catch(e => {
+            console.log(e);
+            channel.send("¿Realmente me has enviado una imagen? Ha pasado algo inesperado.")
+        }
+    );
 
     if (res.length > 0) {
+        channel.send("Esto ha sido lo más parecido que he encontrado");
         res.sort(((a, b) => b.header.similarity - a.header.similarity))
-        let keys = Object.keys(res[0].data);
-        let embed = new Discord.MessageEmbed();
-        for (let i = 0; i < keys.length; i++) {
-            let key = keys[i];
-            if (Array.isArray(res[0].data[key])) {
-                embed.addField(key.replace("_", " "), res[0].data[key].join("\n"), true);
-            } else {
-                embed.addField(key, res[0].data[key], true);
+        for (let i = 0; i < 3 || i < res.length; i++) {
+            let keys = Object.keys(res[i].data);
+            let embed = new Discord.MessageEmbed();
+            for (let i = 0; i < keys.length; i++) {
+                let key = keys[i];
+                if (Array.isArray(res[i].data[key])) {
+                    embed.addField(key.replace("_", " "), res[i].data[key].join("\n"), true);
+                } else {
+                    embed.addField(key.replace("_", " "), res[i].data[key], true);
+                }
+                channel.send(embed);
             }
         }
-        channel.send("Esto ha sido lo más parecido que he encontrado");
-        channel.send(embed);
     } else {
         channel.send("No he podido encontrar nada.");
     }
